fd15bc3f44b7195f52965aed648b9de58c231698
Stuart Harrison <pezholio@gmail.com>
2021-03-04 10:10:38 +0000

    Handle error message from API

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,26 +9,28 @@ output=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $PERSONAL_ACCESS_TOKEN" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls")
+error_message=$(echo "$output" | jq "try .message")
 
-echo "$output"
+if [[ $error_message ]]; then
+  echo "Error response from Github API was $error_message"
+  exit 1
+else
+  PULLS=$(echo "$output" | jq --raw-output '.[] | @base64')
 
-PULLS=$(curl \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $PERSONAL_ACCESS_TOKEN" \
-  "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls" | jq --raw-output '.[] | @base64')
+  for pull in $PULLS; do
+    json=$(echo -n "$pull" | base64 -d)
+    auto_merge=$(echo "$json" | jq -c '.auto_merge')
+    ref=$(echo "$json" | jq -c '.head.ref')
+    pull_request_number=$(echo "$json" | jq -c '.number')
 
-for pull in $PULLS; do
-  json=$(echo -n "$pull" | base64 -d)
-  auto_merge=$(echo "$json" | jq -c '.auto_merge')
-  ref=$(echo "$json" | jq -c '.head.ref')
-  pull_request_number=$(echo "$json" | jq -c '.number')
+    if [[ "$auto_merge" != "null" ]]; then
+      curl \
+        -X POST \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: token $PERSONAL_ACCESS_TOKEN" \
+        "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$WORKFLOW/dispatches" \
+        -d "{\"ref\": $ref, \"inputs\": { \"pr_number\": \"$pull_request_number\"}}"
+    fi
+  done
+fi
 
-  if [[ "$auto_merge" != "null" ]]; then
-    curl \
-      -X POST \
-      -H "Accept: application/vnd.github.v3+json" \
-      -H "Authorization: token $PERSONAL_ACCESS_TOKEN" \
-      "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$WORKFLOW/dispatches" \
-      -d "{\"ref\": $ref, \"inputs\": { \"pr_number\": \"$pull_request_number\"}}"
-  fi
-done
